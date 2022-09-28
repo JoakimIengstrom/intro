1dd61b1b7a449aa590538553b57b27cf506c37c4
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-09-28 13:43:42 +0200

    Set up Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Info from "./components/Info";
+//import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="logo"></header>
+      <body>
+        <h1>Sommarfisk </h1>
+        <Info />
+        <div className="claria"></div>
+        <Info />
+      </body>
     </div>
   );
 }
