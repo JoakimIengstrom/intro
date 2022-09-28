8f5c774b12b8685a9359159a44fb38f770a7f8e8
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-09-28 15:24:16 +0200

    Finish up for the day with just som classnames.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
 import Info from "./components/Info";
 import Form from "./components/Form";
+import FormTest from "./components/FormTest";
 
 function App() {
   return (
     <div className="App">
-      <header className="logo"></header>
+      <header className="fishHeader">
+        <div className="logo"></div>
+        <h1 className="headline">Sommarfisk </h1>
+      </header>
       <body>
-        <h1>Sommarfisk </h1>
         <Info />
         <div className="claria"></div>
         <Info />
         <Form />
+        <FormTest />
       </body>
     </div>
   );
