3fc0ad7d2f2412e1007f663b0677a201aee087d7
AdrianRydin <adrian.rydin@gmail.com>
2022-10-04 18:08:52 +0200

    Changed font's from className to id
    Also did some small changes

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -5,17 +5,24 @@ import "./Styling/grid.css"
 
 export default function Facts() {
   return (
-    <div className='flex justify-center align-center text-center flex-column factDiv facts'>
+    <div className='flex justify-center flex-column factDiv'>
         <h2 id="factSubTitle">Visste du att?</h2>
-        <p id="infoText">Clarias kan - till skillnad från andra arter -  leva tätt. De gillar sällskap!</p>
-        <p id="infoText">Clarias Är en proteinmästare. från 100kg foder får man 90kg fisk. en höna ger 40kg.</p>
-        <p id="infoText">Clarias serveras på några av sveriges bästa restauranger: <br /><br />  
+        <p id="Paragraph-3">Clarias kan - till skillnad från andra arter -  leva tätt. De gillar sällskap! <br /> <br /> <br />
+          Clarias Är en proteinmästare. från 100kg foder får man 90kg fisk. en höna ger 40kg. <br /> <br />
+      </p>
+            <p id='Paragraph-3'>
+             Clarias serveras på några av sveriges bästa restauranger:
+            
             <b>Koizen</b> i Göteborg <br />
             <b>Soyokazes</b> I Stockholm  <br />
             <b>Pm <b id="specialSign">&</b> Vänner</b>* i Växjö
+              
+            </p>
+         
+        <p id="Paragraph-3">Systemet har möjlighet att användas på platser där det är brist på mat. <br /> <br />
+        
+          på några få kvadratmeter kan man få 150kg fisk under en sommar - crazy!
         </p>
-        <p id="infoText">Systemet har möjlighet att användas på platser där det är brist på mat.</p>
-        <p id="infoText">på några få kvadratmeter kan man få 150kg fisk under en sommar - crazy!</p>
     </div>
   )
 }
